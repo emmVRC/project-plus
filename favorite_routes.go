aee5e436ed9090c14af77c75ffbe57666d7bdaa3
BenjaminZehowlt <16182181+BenjaminZehowlt@users.noreply.github.com>
2022-06-14 05:03:59 -0400

    Sigh...

--- favorite_routes.go
+++ favorite_routes.go
@@ -140,7 +140,7 @@ func DeleteAvatarFavorite(c *fiber.Ctx) error {
 	tx := DatabaseConnection.Where("user_id = ? AND avatar_id = ?", userId, f.AvatarId).First(&a)
 
 	if tx.Error == nil {
-		tx = DatabaseConnection.Unscoped().Delete(&a)
+		tx = DatabaseConnection.Delete(&a)
 
 		if tx.Error != nil {
 			return c.Status(http.StatusInternalServerError).JSON(ErrInternalServerError)
